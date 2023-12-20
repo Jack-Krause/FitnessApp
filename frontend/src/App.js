b7cfb1fa6963d0d6afe1882458780bd5404d5091
Jack Krause <jmkrause@iastate.edu>
2023-12-20 15:22:32 -0600

    frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React, { useState, useEffect } from "react";
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //context wrap
+    <Router>
+      <div className = "App">
+        <NavBar />
+        <Routes>
+          <Route path = "/" element = { <HomeView />} />
+          
+
+        </Routes>
+
+      </div>
+    </Router>
+
   );
-}
+};
 
 export default App;
