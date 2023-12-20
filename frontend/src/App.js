df5defcabb0e73529923442cbd9e15ba4d9ae3a4
Jack Krause <jmkrause@iastate.edu>
2023-12-20 15:26:33 -0600

    frontend setup finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import './index.css';
 import React, { useState, useEffect } from "react";
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    //context wrap
+    // context wrap here if needed
     <Router>
       <div className = "App">
         <NavBar />
         <Routes>
           <Route path = "/" element = { <HomeView />} />
-          
+
 
         </Routes>
 
