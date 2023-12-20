467c90a1112f6cc61a12c499f7cca6cc5f4e8625
Jack Krause <jmkrause@iastate.edu>
2023-12-20 17:45:22 -0600

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,20 @@ import './index.css';
 import React, { useState, useEffect } from "react";
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import views:
+import NavHead from "./components/NavHead";
 
 const App = () => {
   return (
     // context wrap here if needed
     <Router>
       <div className = "App">
-        <NavBar />
-        <Routes>
+        <NavHead />
+        {/* <Routes>
           <Route path = "/" element = { <HomeView />} />
 
 
-        </Routes>
+        </Routes> */}
 
       </div>
     </Router>
---
+++ frontend/src/components/NavHead.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavHead() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Fitness Tracker By Jack</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/">Workout</Nav.Link>
+            <NavDropdown title="See More:" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavHead;
