ca3200c94f1b9ee8ec610cb4c6205251b2691278
Jack Krause <jmkrause@iastate.edu>
2024-01-02 15:33:15 -0600

    ui for exercise browse

--- frontend/src/components/Workout.js
+++ frontend/src/components/Workout.js
@@ -1,8 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
+import { useAuthenticatedView } from "../contexts/AuthenticatedViewProvider";
 import { getExercise } from "../api/requestMethods";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Workout = () => {
+  const { userEmail } = useAuthenticatedView();
+
   const [exercises, setExercises] = useState([]);
 
   useEffect(() => {
@@ -24,12 +30,45 @@ const Workout = () => {
 
   return (
     <div>
-      {/* Display exercises or use the data as needed */}
-      {exercises.map((exercise) => (
-        <div key={exercise.id}>{exercise.name}</div>
-      ))}
+      <main>
+        <Container fluid className="p-3 bg-black">
+          <Row className="justify-content-center mt-4">
+            <Col>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="150"
+                length="auto"
+                fill="white"
+                className="mt-4 mb-4 bi bi-backpack"
+                viewBox="0 0 16 16"
+              >
+                <path d="M4.04 7.43a4 4 0 0 1 7.92 0 .5.5 0 1 1-.99.14 3 3 0 0 0-5.94 0 .5.5 0 1 1-.99-.14M4 9.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5zm1 .5v3h6v-3h-1v.5a.5.5 0 0 1-1 0V10z" />
+                <path
+                  fillRule="evenodd"
+                  d="M6 2.341V2a2 2 0 1 1 4 0v.341c2.33.824 4 3.047 4 5.659v5.5a2.5 2.5 0 0 1-2.5 2.5h-7A2.5 2.5 0 0 1 2 13.5V8a6.002 6.002 0 0 1 4-5.659M7 2v.083a6.04 6.04 0 0 1 2 0V2a1 1 0 0 0-2 0m1 1a5 5 0 0 0-5 5v5.5A1.5 1.5 0 0 0 4.5 15h7a1.5 1.5 0 0 0 1.5-1.5V8a5 5 0 0 0-5-5"
+                />
+              </svg>
+            </Col>
+            <Col>
+              <h1 className="mt-4 mb-4 text-light text-general">
+                Browse Exercises
+              </h1>
+            </Col>
+          </Row>
+          {/* end row 1 */}
+        </Container>
+      </main>
     </div>
   );
+
+  // return (
+  //   <div>
+  //     {/* Display exercises or use the data as needed */}
+  //     {exercises.map((exercise) => (
+  //       <div key={exercise.id}>{exercise.name}</div>
+  //     ))}
+  //   </div>
+  // );
 };
 
 export default Workout;
