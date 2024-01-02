477d6927e077504cd16433b9a503897445147595
Jack Krause <jmkrause@iastate.edu>
2024-01-02 14:08:48 -0600

    basic public api methods

--- frontend/src/api/requestMethods.js
+++ frontend/src/api/requestMethods.js
@@ -3,31 +3,47 @@ import axios from "axios";
 
 // GET request to sign in user
 export const signIn = async (inputEmail, inputPassword) => {
-    try {
-        const response = await axios.post("http://127.0.0.1:4000/user/signin", {
-            email: inputEmail,
-            password: inputPassword
-        });
-        return response;
-    } catch (err) {
-        console.error("[api] error", err);
-        throw err;
-    }
+  try {
+    const response = await axios.post("http://127.0.0.1:4000/user/signin", {
+      email: inputEmail,
+      password: inputPassword,
+    });
+    return response;
+  } catch (err) {
+    console.error("[api] error", err);
+    throw err;
+  }
 };
 
 // POST request to add user info
 export const register = async (inputEmail, inputPassword, inputName) => {
-    try {
-        console.log(inputEmail, inputPassword, inputName);
-        const response = await axios.post("http://127.0.0.1:4000/user/register", {
-            email: inputEmail,
-            password: inputPassword,
-            name: inputName
-        });
+  try {
+    console.log(inputEmail, inputPassword, inputName);
+    const response = await axios.post("http://127.0.0.1:4000/user/register", {
+      email: inputEmail,
+      password: inputPassword,
+      name: inputName,
+    });
 
-        return response;
-    } catch (err) {
-        console.error("[api] error", err);
-        throw err;
-    }
+    return response;
+  } catch (err) {
+    console.error("[api] error", err);
+    throw err;
+  }
+};
+
+export const getExercise = async (exercise) => {
+  try {
+    const response = await axios.get("https://wger.de/api/v2/exercise/search", {
+      params: {
+        language: "en",
+        term: exercise,
+      },
+    });
+
+    return response;
+  } catch (err) {
+    console.error("[public api error]", err);
+    throw err;
+  }
 };
--- frontend/src/components/Workout.js
+++ frontend/src/components/Workout.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { getExercise } from "../api/requestMethods";
 
 const Workout = () => {
+    const [exercises, setExercises] = useState([]);
+
+    useEffect(() => {
+        const fetchExerciseData = async () => {
+            try {
+                const response = await getExercise("bench press");
+                if (response.data.suggestions) {
+                    setExercises(response.data.suggestions.map(item => item.data));
+                } else {
+                    console.error("Unexpected response format:", response);
+                }
+            } catch (err) {
+                console.error("[public api error]", err);
+            }
+        };
+
+        fetchExerciseData();
+    }, []);
+
     return (
         <div>
-
+            {/* Display exercises or use the data as needed */}
+            {exercises.map((exercise) => (
+                <div key={exercise.id}>{exercise.name}</div>
+            ))}
         </div>
     );
 };
-
 
 export default Workout;
